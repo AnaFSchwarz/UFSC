e75043565da5ea9bc377d680c5d61ff59a481366
AnaFSchwarz <94197112+AnaFSchwarz@users.noreply.github.com>
2022-09-14 20:33:54 -0300

    Delete teste.js

--- STT/teste.js
+++
@@ -1,8 +0,0 @@
-function verificaResposta() {
-    const resposta = formulario.sistema.value
-    if (resposta == "android") {
-        document.getElementById("oi").innerHTML = "Seu celular trava"
-    } else {
-        document.getElementById("oi").innerHTML = "Ta acabando sua bateria"
-    }
-}
